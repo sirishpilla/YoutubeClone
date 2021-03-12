73dff5395082ea17341c056a0bff00e225656855
Sirish Pilla <sirishpilla@gmail.com>
2021-03-11 23:02:45 -0600

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //Create a new component. This component should produce some html
 const App=function(){
-    return <div>Hi !</div>
+    return <div>Hi Sirish !</div>
 }
 
 //Take this component generated html and put it on page(DOM)
